44b76e93f642d443905bf3041c1c130108f17ff2
salumalove <31348743+salumalove@users.noreply.github.com>
2017-08-28 22:02:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,33 +27,33 @@ function createTemplate (data) {
     var content= data.content;
 
 var htmlTemplate = `
-<html>
-    <head>
-        <title>
-            ${tite}]
-        </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <link href="/ui/style.css" rel="stylesheet" /> 
-    </head>
-    <body>
-        <div class="container">
-            <div>
-                <a href="/">Home</a>
-            </div>    
-            <hr/>
-            <h3>
-                ${heading}
-            </h3>
-            <div>
-               ${date}
+    <html>
+        <head>
+            <title>
+                ${tite}]
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" /> 
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>    
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                   ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
             </div>
-            <div>
-                ${content}
-            </div>
-        </div>
-    </body>
-</html>
-`;
+        </body>
+    </html>
+    `;
 return htmlTemplate;    
 }
 
@@ -66,7 +66,7 @@ app.get('/article-one',function(req,res){
 });
 
 app.get('/article-two',function(req,res){
- res.sendFile(path.join(__dirname, 'ui', 'article-2.html'));
+ res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-three',function(req,res){
